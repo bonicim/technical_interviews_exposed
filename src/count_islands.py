2222483826ce063748e6d2b7a57cf948a97c9ec1
Mark Bonicillo <markabonicillo@gmail.com>
2019-03-02 16:41:13 -0800

    Implement solution, all tests passing

--- src/count_islands.py
+++ src/count_islands.py
@@ -1,2 +1,52 @@
 def count_islands(mmap):
-    return None
+    max = 0
+    for row, row_elem in enumerate(mmap):
+        for col, col_elem in enumerate(row_elem):
+            if col_elem == "L":
+                size = 1
+                row_elem[col] = "O"
+                size = get_island_size(mmap, row, col, size)
+                if size > max:
+                    max = size
+    return max
+
+def get_island_size(mmap, row, col, initial_size):
+    total_size = initial_size
+
+    # check west
+    if col - 1 == -1:
+        print("Not on real west. Out of bounds")
+    elif mmap[row][col - 1] == "O":
+        print("On ocean. No land. Stop checking and check next adjacent west.")
+    else:
+        print("On land. Increase size to 1 and check the adjacent spots of this west.")
+        mmap[row][col - 1] = "O"
+        total_size = total_size + 1
+        total_size = get_island_size(mmap, row, col - 1, total_size)
+    
+    # check north
+    if row - 1 == -1:
+        print("Not on real north. Out of bounds.")     
+    elif mmap[row - 1][col] == "L":
+        mmap[row - 1][col] = "O"
+        total_size = total_size + 1
+        total_size = get_island_size(mmap, row - 1, col, total_size)
+
+    # check east
+    if col + 1 > len(mmap[row]) - 1:
+        print("Not on real east. Out of bounds")
+    elif mmap[row][col + 1] == "L":
+        mmap[row][col + 1] = "O"
+        total_size = total_size + 1
+        total_size = get_island_size(mmap, row, col + 1, total_size)
+
+    # check south
+    if row + 1 > len(mmap) - 1:
+        print("Not on real south. Out of bounds.")  
+    elif mmap[row + 1][col] == "L":
+        mmap[row + 1][col] = "O"
+        total_size = total_size + 1
+        total_size = get_island_size(mmap, row + 1, col, total_size)
+
+    return total_size
+
