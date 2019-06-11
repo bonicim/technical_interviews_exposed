72560fb41f74b9baa3635afcaef968d59f7034e4
Mark Bonicillo <markabonicillo@gmail.com>
2019-06-10 22:41:54 -0700

    Update tests

--- tst/test_get_nearest_ancestor.py
+++ tst/test_get_nearest_ancestor.py
@@ -3,7 +3,15 @@ from src.algorithms.get_nearest_ancestor import get_nearest_ancestor
 from src.algorithms.node import Node
 
 def test_typical_case():
-    root = Node(20)
-    root.left = Node(10)
-    root.right = Node(30)
-    assert get_nearest_ancestor(root, 10, 30) == root
+    assert get_nearest_ancestor(root, 10, 30) == Node(20)
+
+def test_left_branch():
+    assert get_nearest_ancestor(root, 5, 19) == Node(10)
+
+root = Node(20)
+root.left = Node(10)
+root.right = Node(30)
+root.left.left = Node(5)
+root.left.right = Node(19)
+root.right.left = Node(25)
+root.right.right = Node(41)
--- tst/test_get_nearest_ancestor_v2.py
+++ tst/test_get_nearest_ancestor_v2.py
@@ -3,21 +3,21 @@ from src.algorithms.get_nearest_ancestor_v2 import get_nearest_ancestor_v2
 from src.algorithms.node import Node
 
 def test_easy_case():
-    assert get_nearest_ancestor_v2(left_child, right_child) == root
+    assert get_nearest_ancestor_v2(left_child, right_child) == Node(42)
 
 def test_ancerstor_2nd_level():
-    assert get_nearest_ancestor_v2(child1, child2) == left_child
+    assert get_nearest_ancestor_v2(child1, child2) == Node(13)
 
 def test_ancerstor_unbalanced():
-    assert get_nearest_ancestor_v2(child1, child2_1_1) == left_child
+    assert get_nearest_ancestor_v2(child1, child2_1_1) == Node(13)
 
 def test_ancestor_unbalanced_v2():
-    assert get_nearest_ancestor_v2(right_child, child2_1_1) == root
+    assert get_nearest_ancestor_v2(right_child, child2_1_1) == Node(42)
 
 def test_ancestor_unbalanced_v3():
-    assert get_nearest_ancestor_v2(child2_1, child2_1_1) == child2
+    assert get_nearest_ancestor_v2(child2_1, child2_1_1) == Node(7)
 
-root = Node(42, parent=None)
+root = Node(42)
 left_child = Node(13, parent=root)
 right_child = Node(15, parent=root)
 root.left = left_child
