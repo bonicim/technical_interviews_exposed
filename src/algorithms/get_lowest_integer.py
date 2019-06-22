f76ed23e6c251b79fa0c2528c1aa23b92b110533
Mark Bonicillo <markabonicillo@gmail.com>
2019-06-22 14:45:23 -0700

    Fix bug in get_lowest_integer and add more tests

--- src/algorithms/get_lowest_integer.py
+++ src/algorithms/get_lowest_integer.py
@@ -1,15 +1,22 @@
 def get_lowest_integer(number, remove):
-    if len(number) <= remove:
-        return "0"
-    
     digits = [int(digit) for digit in number]    
     stack = [digits.pop(0)]
 
     for digit in digits:
         for num in stack[::-1]:
-            if remove > 0 and digit < num:
-                stack.pop()    
+            if (remove > 0) and (digit < num):
+                stack.pop()
                 remove -= 1
-        stack.append(digit)            
+        stack.append(digit)
 
+    # handles edge cases where largest digits are in the rightmost part of the number
+    if remove > 0:
+        while stack and remove > 0 :
+            stack.pop()
+            remove -= 1
+
+    # required to ensure that 0 is returned when removing all the digits
+    if not stack:
+        stack.append("0")
+ 
     return ''.join([str(x) for x in stack])
