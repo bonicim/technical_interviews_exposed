5384287ad97e4b99b21b05c28cac991179d4a061
Mark Bonicillo <markabonicillo@gmail.com>
2019-07-28 14:36:01 -0700

    Add pigeonhole principle sol to Missing Integer

--- src/algorithms/find_missing_integer.py
+++ src/algorithms/find_missing_integer.py
@@ -1,4 +1,7 @@
 def find_missing_integer(arr):
+    return find_missing_integer_pigeonhole_principle_sol(arr)
+
+def find_missing_integer_set(arr):
     result = 1
     arr_set = set(arr)
     
@@ -6,3 +9,16 @@ def find_missing_integer(arr):
         if (num > 0) and (result in arr_set):
             result += 1
     return result
+
+def find_missing_integer_pigeonhole_principle_sol(arr):
+    is_present = [False] * (len(arr) + 1)
+
+    for num in arr:
+        if 0 < num <= len(is_present) - 1:
+            is_present[num] = True
+    
+    for num in range(1, len(is_present)):
+        if not is_present[num]:
+            return num
+    
+    return len(is_present)
