25b0c323b97af56534fbcbce8cab342e2c572873
Mark Bonicillo <markabonicillo@gmail.com>
2019-08-03 15:52:06 -0700

    Add comments to tape_equilibrium and update tests

--- tst/test_tape_equilibrium.py
+++ tst/test_tape_equilibrium.py
@@ -1,6 +1,14 @@
 import pytest
 from src.algorithms.tape_equilibrium import get_min_from_tape
 
-def test_regular_case():
+def test_regular():
     arr = [3, 1, 2, 4, 3]
     assert get_min_from_tape(arr) == 1
+
+def test_sorted_positive():
+    arr = [1, 2, 3]
+    assert get_min_from_tape(arr) == 0
+
+def test_one_negative_case():
+    arr = [-100, 50, 60, 10]
+    assert get_min_from_tape(arr) == 0
