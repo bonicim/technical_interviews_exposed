af5eba944233817daec105e1e804972b4db7ff39
Mark Bonicillo <markabonicillo@gmail.com>
2019-08-13 00:05:08 -0700

    Add binary-search hash solution

--- src/algorithms/two_sum.py
+++ src/algorithms/two_sum.py
@@ -14,7 +14,7 @@ def two_sum(n, k):
     :raises: None
 
     """
-    return two_sum_hash_sol(n, k)
+    return two_sum_sort_binary_search_sol(n, k)
 
 def two_sum_brute_force_sol(n, k):
     for i, val in enumerate(n):
@@ -43,7 +43,28 @@ def two_sum_hash_sol(n, k):
     return False
 
 def two_sum_sort_binary_search_sol(n, k):
-    return
+    n_sorted = sorted(n)
+    
+    for i, val in enumerate(n):
+        diff_idx = binary_search(0, len(n_sorted) - 1, k - val, n_sorted)
+        if diff_idx >= 0:
+            if (diff_idx != i or 
+                    (i > 0 and (n_sorted[i - 1] == n_sorted[i])) or 
+                    (i < len(n_sorted) - 1 and (n_sorted[i + 1] == n_sorted[i]))):
+                return True        
+    return False
+
+def binary_search(start, end, tgt, n_sorted):
+    while (start <= end):
+        mid = int((start + end) / 2)
+        if tgt < n_sorted[mid]:
+            end = mid - 1
+        elif tgt > n_sorted[mid]:
+            start = mid + 1
+        elif n_sorted[mid] == tgt:
+            return mid
+    return -1       
+
 
 def two_sum_sort_walk_inward_sol(n, k):
     return
