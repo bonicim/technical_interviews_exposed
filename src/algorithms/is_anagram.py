4cda0f0dff20d656d22ec4121b2923a30cd9c387
Mark Bonicillo <markabonicillo@gmail.com>
2019-08-14 20:33:20 -0700

    Add stubs for anagram problem

---
+++ src/algorithms/is_anagram.py
@@ -0,0 +1,14 @@
+def is_anagram(w1, w2):
+    return is_anagram_brute_force(w1, w2)
+
+def is_anagram_brute_force(w1, w2):
+    return
+
+def is_anagram_sort(w1, w2):
+    return
+
+def is_anagram_count_char(w1, w2):
+    return
+
+def is_anagram_histograms(w1, w2):
+    return
