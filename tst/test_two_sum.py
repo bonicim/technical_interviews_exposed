8320001202dbe1fea83e056d9f0dc525af2141d1
Mark Bonicillo <mark.a.bonicillo@gmail.com>
2019-09-23 23:57:45 -0700

    Add Hypothesis as testing requirement; add example

--- tst/test_two_sum.py
+++ tst/test_two_sum.py
@@ -1,13 +1,22 @@
 import pytest
+import hypothesis.strategies as st
 
 from src.algorithms.two_sum import two_sum
+from hypothesis import given
+
+
+@given(st.lists(st.integers(), max_size=0), st.integers())
+def test_empty_list(x, y):
+    assert two_sum(x, y) is False
+
 
 def test_example():
     arr = [2, 7, 11, 15]
     tgt = 9
     actual = two_sum(arr, tgt)
-    
+
     assert actual is True
+
 
 def test_empty():
     arr = []
@@ -16,12 +25,14 @@ def test_empty():
 
     assert actual is False
 
+
 def test_single():
     arr = [1]
     tgt = 2
     actual = two_sum(arr, tgt)
 
     assert actual is False
+
 
 def test_duplicate():
     arr = [2, 2, 3]
@@ -30,12 +41,14 @@ def test_duplicate():
 
     assert actual is True
 
+
 def test_negative_integers_included():
     arr = [9, -6, -7, 6, 3]
     tgt = -13
     actual = two_sum(arr, tgt)
 
     assert actual is True
+
 
 def test_half_target_included():
     arr = [9, 7, 3]
@@ -44,12 +57,14 @@ def test_half_target_included():
 
     assert actual is False
 
+
 def test_sorted_ascending():
     arr = [x for x in range(10)]
     tgt = 17
     actual = two_sum(arr, tgt)
 
     assert actual is True
+
 
 def test_sorted_descending():
     n = [x for x in range(9, -1, -1)]
@@ -58,12 +73,14 @@ def test_sorted_descending():
 
     assert actual is True
 
+
 def test_only_neg():
     n = [-23434, -2, -4, -16, -5]
     k = -21
     actual = two_sum(n, k)
 
     assert actual is True
+
 
 def test_zero_only():
     actual = two_sum([0], -343)
