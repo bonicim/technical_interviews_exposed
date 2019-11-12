114b718de761b1d3630f21b2bfd92a1779314e40
Mark Bonicillo <markabonicillo@gmail.com>
2019-11-12 13:16:05 -0800

    Update is_palindrome solutions and test
    Signed-off-by: Mark Bonicillo <markabonicillo@gmail.com>

--- src/algorithms/is_palindrome.py
+++ src/algorithms/is_palindrome.py
@@ -1,10 +1,72 @@
 def is_palindrome(string):
-    return is_palindrome_helper(string, 0, len(string) - 1)
+    # return is_palindrome_walk_inward(string)
+    return is_palindrome_recursive_solution(string, 0, len(string) - 1)
 
 
-def is_palindrome_helper(string, left_index, right_index):
-    if len(string) == 0 or left_index == right_index:
+def is_palindrome_recursive_solution(string, left_index, right_index):
+    if len(string) == 0 or left_index == right_index or left_index > right_index:
         return True
-    if string[left_index] != string[right_index]:
+
+    string = string.lower()
+    left_char = string[left_index]
+    right_char = string[right_index]
+
+    while not left_char.isalnum() and left_index < right_index:
+        left_index += 1
+        left_char = string[left_index]
+    if not left_char.isalnum():
+        left_char = ""
+
+    while not right_char.isalnum():
+        if right_index == 0:
+            break
+        right_index -= 1
+        right_char = string[right_index]
+    if not right_char.isalnum():
+        right_char = ""
+
+    if left_char != right_char:
         return False
-    return is_palindrome_helper(string, left_index + 1, right_index -1)
+
+    return is_palindrome_recursive_solution(string, left_index + 1, right_index - 1)
+
+
+def is_palindrome_walk_inward(string):
+    alphanum = set()
+    for i in range(10):
+        alphanum.add(str(i))
+    for i in range(ord("a"), ord("z") + 1):
+        alphanum.add(chr(i))
+
+    # yes, I could have used the builtin function isalnum but I wanted to implement my version simply for learning
+    def next_alphanumeric_character(string, index, start_from_left=True):
+        char = string[index]
+        if start_from_left:
+            while char not in alphanum and index < len(string) - 1:
+                index += 1
+                char = string[index]
+        else:
+            while char not in alphanum and index >= 0:
+                index -= 1
+                char = string[index]
+
+        if not char.isalnum():
+            char = ""
+
+        return string[index], index
+
+    left = 0
+    right = len(string) - 1
+    string = string.lower()
+
+    while left < right:
+        left_char, left = next_alphanumeric_character(string, left)
+        right_char, right = next_alphanumeric_character(
+            string, right, start_from_left=False
+        )
+        if left_char == right_char:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
--- tst/test_is_palindrome.py
+++ tst/test_is_palindrome.py
@@ -2,5 +2,34 @@ import pytest
 from src.algorithms.is_palindrome import is_palindrome
 
 
-def test_true():
-    assert is_palindrome("aba") is True
+def test_should_be_true():
+    string = "A man, a plan, a canal: Panama"
+    assert is_palindrome(string) is True
+
+    string = "AmanaplanacanalPanama"
+    assert is_palindrome(string) is True
+
+    assert is_palindrome("111121111") is True
+
+
+def test_should_be_false():
+    string = "race a car"
+    assert is_palindrome(string) is False
+
+    assert is_palindrome("112") is False
+
+
+def test_only_non_alpha_numeric_should_be_true():
+    assert is_palindrome(":") is True
+    assert is_palindrome(" ") is True
+
+
+def test_empty_string_should_be_true():
+    assert is_palindrome("") is True
+
+
+def test_non_alpha_numeric_should_be_true():
+    assert is_palindrome(",;") is True
+    assert is_palindrome("%") is True
+    assert is_palindrome(",,,,,,,,,") is True
+    assert is_palindrome(",;^$#") is True
