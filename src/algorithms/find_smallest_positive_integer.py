e155156ce4fa55a53784bbf5f51e584a59425497
Mark Bonicillo <markabonicillo@gmail.com>
2019-11-26 10:49:50 -0800

    Rename find_missing_integer; update requirements.txt

--- src/algorithms/find_smallest_positive_integer.py
+++ src/algorithms/find_smallest_positive_integer.py
@@ -1,15 +1,26 @@
-def find_missing_integer(arr):
+def find_smallest_positive_integer(arr):
+    """
+    Given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+    For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+    Given A = [1, 2, 3], the function should return 4.
+
+    Given A = [−1, −3], the function should return 1.
+    """
     # return find_missing_integer_set(arr)
     return find_missing_integer_pigeonhole_principle_sol(arr)
+
 
 def find_missing_integer_set(arr):
     result = 1
     arr_set = set(arr)
-    
+
     for num in arr_set:
         if (num > 0) and (result in arr_set):
-         result += 1
+            result += 1
     return result
+
 
 def find_missing_integer_pigeonhole_principle_sol(arr):
     is_present = [False] * (len(arr) + 1)
@@ -17,9 +28,9 @@ def find_missing_integer_pigeonhole_principle_sol(arr):
     for num in arr:
         if 0 < num <= len(is_present) - 1:
             is_present[num] = True
-    
+
     for num in range(1, len(is_present)):
         if not is_present[num]:
             return num
-    
+
     return len(is_present)
--- tst/test_find_missing_integer.py
+++ tst/test_find_missing_integer.py
@@ -1,112 +1,130 @@
 import pytest
 import random
 
-from src.algorithms.find_missing_integer import find_missing_integer
+from src.algorithms.find_smallest_positive_integer import find_smallest_positive_integer
+
 
 def test_example_1():
     arr = [1, 3, 6, 4, 1, 2]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 5
+
 
 def test_example_2():
     arr = [-1, -3]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_example_3():
     arr = [1, 3, 2]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 4
+
 
 def test_simple():
     arr = [2, 3, 4, 5]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_all_neg():
     arr = [-1, -3, -2]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_simple_single():
     arr = [2]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_simple_single_v2():
     arr = [1]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 2
+
 
 def test_simple_single_zero():
     arr = [0]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_simple_single_neg():
     arr = [-1]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_extreme_positive_single():
     arr = [1000000]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_extreme_negative_single():
     arr = [-1000000]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_extreme_min_max():
     arr = [1000000, -1000000]
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_shuffled_positive_only():
     arr1 = [x for x in range(11)]
     arr2 = [x for x in range(12, 21)]
     arr = arr1 + arr2
     random.shuffle(arr)
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 11
+
 
 def test_shuffled_negative_only():
     arr = [x for x in range(-1, -11, -1)]
     random.shuffle(arr)
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 1
+
 
 def test_shuffled_pos_neg_zero():
     arr1 = [x for x in range(-1, -11, -1)]
     arr2 = [x for x in range(1, 101)]
     arr = arr1 + arr2
     random.shuffle(arr)
-    actual = find_missing_integer(arr)
-    
+    actual = find_smallest_positive_integer(arr)
+
     assert actual == 101
+
+
 def test_empty_list():
     arr = []
-    actual = find_missing_integer(arr)
+    actual = find_smallest_positive_integer(arr)
 
     assert actual == 1
 
+
 def test_duplicates_list():
     arr = [5, 5, 5, 5]
-    actual = find_missing_integer(arr)
+    actual = find_smallest_positive_integer(arr)
 
     assert actual == 1
