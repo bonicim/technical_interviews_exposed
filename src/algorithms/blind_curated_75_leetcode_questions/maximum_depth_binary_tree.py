4b675b54229c395c351e2ed539a89d6c4f55b83d
Mark Bonicillo <markabonicillo@gmail.com>
2020-01-15 16:31:54 -0800

    Add commentary to max depth binary tree

--- src/algorithms/blind_curated_75_leetcode_questions/maximum_depth_binary_tree.py
+++ src/algorithms/blind_curated_75_leetcode_questions/maximum_depth_binary_tree.py
@@ -1,3 +1,34 @@
+"""
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+
+"""Commentary
+
+This is a classic tree searching question that tests if you know DFS and know how to code it up quickly. The algorithm
+simply is to get the height of the left and right side. Compare them. And choose the bigger height. Very simple.
+
+The key to this is recursion. You want to use the call stack (not a stack data structure, although you can) to hold your
+recursive calls. You need to know what is the base case (aka stopping point). And you need to know how to use accumulators implicitly
+to get the total height of each side.
+
+"""
+
+
 def max_depth(root):
     if not root:
         return 0
