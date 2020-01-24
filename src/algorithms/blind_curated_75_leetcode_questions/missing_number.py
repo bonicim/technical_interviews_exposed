2d31f3d6f119deb51b1a0811edebca4721405e54
Mark Bonicillo <markabonicillo@gmail.com>
2020-01-24 12:05:45 -0800

    Add commentary to missing_number

--- src/algorithms/blind_curated_75_leetcode_questions/missing_number.py
+++ src/algorithms/blind_curated_75_leetcode_questions/missing_number.py
@@ -1,9 +1,26 @@
-def find_missing_integer_ii(nums):
-    """Given a list of unsorted, distinct, contiguous integers, return the value of the missing integer that would
-    make the list fully contiguous. The list contains only non-negative integers.
+"""Given a list of unsorted, distinct, contiguous integers, return the value of the missing integer that would
+make the list fully contiguous. The list contains only non-negative integers.
 
-    Example:  [ 2, 3, 0] => 1
-    """
+Example:  [ 2, 3, 0] => 1
+"""
+
+""" Commentary
+
+To truly understand this problem, we have to recognize and understand the input. The input is a list of
+unique numbers such that they can be arranged monotonically, save for one. For example, [3,1,0] is equiavalent
+to [0,1,3], with 2 being the missing number. We can take advantage of this idea by using the indexes of the
+array that hold these numbers. If we sorted the list, then we simply check the index agains the actual value for the missing number.
+
+But if we didn't want to sort, we can use a set to store all the values of the list and then simply
+iterate through all the index of the length of the list.
+
+Finally, this problem alludes to the mathematical property of triangular sums, which can be used to
+solve this problem very quickly.
+"""
+
+
+def find_missing_integer_ii(nums):
+
     # return sorted_solution(nums)
     # return hash_set_solution(nums)
     return triangular_series_solution(nums)
@@ -16,14 +33,16 @@ def sorted_solution(nums):
         if index != num:
             return index
 
+    # all the numbers from 0 to n-1 are present, so the missing number is simply the next
+    # number in the serie
+    # the edge case that this catches is [0,1,2]
     return len(nums)
 
 
 def hash_set_solution(nums):
     nums_temp = set(nums)
-
     for index in range(len(nums)):
-        if index not in nums_temp:
+        if index not in nums_temp:  # allows for quick lookup
             return index
 
     return len(nums)
