f25e8415cb72be76c2c6957c236d3394843d684a
Mark Bonicillo <markabonicillo@gmail.com>
2020-01-25 13:21:52 -0800

    Add quicksort

---
+++ src/algorithms/basic_search_algorithms/breadth_first_search.py
@@ -0,0 +1,27 @@
+# BFS is typically used for finding paths, typically the shortest path, so we will track a path here
+def bfs(graph, start, end):
+    discovered = set()
+    discovered.add(start)
+    parents = {}
+    parents.update({start: None})
+    queue = []
+    queue.append(start)
+
+    while queue:
+        current = queue.pop(0)
+        # do some logic on the current node
+        for neighbor in current.neighbors:
+            # do some logic on the current neighbor
+            if neighbor not in discovered:
+                discovered.add(neighbor)
+                parents.update({neighbor: current})
+                queue.append(neighbor)
+
+    return shortest_path(start, end, parents)
+
+
+def shortest_path(start, end, parents):
+    if start == end or not end:
+        return start.data
+
+    return str(end.data) + str(shortest_path(start, parents[end], parents))
---
+++ src/algorithms/basic_sort_algorithms/quicksort.py
@@ -0,0 +1,40 @@
+def quicksort(arr, low, high):
+    if low < high:
+        partition_index = partition(arr, low, high)
+        quicksort(arr, low, partition_index - 1)
+        quicksort(arr, partition_index + 1, high)
+
+
+# the work of quicksort is done in the partition function
+def partition(arr, low, high):
+    # choose some pivot
+    pivot_index = generate_pivot_index(low, high)
+
+    # move the value of the pivot to the far right or the box in the highest index
+    swap(arr, pivot_index, high)
+    pivot_index = high
+
+    # set a pointer on the leftmost part of the array
+    border = low
+
+    for index in range(low, high):
+        # depending on how you want to sort, change the logic in the if statement
+        # depending on where you isolate the pivot value will determine your logic
+        # if the pivot value is on the far right, then you want to operate/move elements that are less than the pivot
+        # if the pivot value is on the far left, then you want to operate/move elements that are greater than the pivot
+        if arr[index] < arr[pivot_index]:
+            swap(arr, index, border)
+            border += 1
+
+    swap(arr, border, pivot_index)
+
+    return border
+
+
+def swap(arr, l, r):
+    arr[l], arr[r] = arr[r], arr[l]
+
+
+def generate_pivot_index(low, high):
+    # implement any logic here
+    return high
