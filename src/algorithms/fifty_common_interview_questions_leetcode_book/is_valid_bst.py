a0924f74ce7c34d5017c744938a77a8332d19fc7
Mark Bonicillo <markabonicillo@gmail.com>
2020-01-27 13:01:54 -0800

    Add commentary to isvalidbst; rename variables and function names

--- src/algorithms/fifty_common_interview_questions_leetcode_book/is_valid_bst.py
+++ src/algorithms/fifty_common_interview_questions_leetcode_book/is_valid_bst.py
@@ -9,23 +9,42 @@ Both the left and right subtrees must also be binary search trees.
 
 """
 
+""" Commentary
+
+The interesting thing about this problem and the solution is that it uses DFS as a strategy to
+determine if a tree is a valid BST. Moreover, in order to know if it is a valid BST, we have to initially
+set arbitrary lower and upper bounds to determine validity at a node. If the node is valid, then
+we need to check the left and right subtree, but with adjusted lower and upper bounds, namely coming from the
+parent node. For example, the left child should be greater than the lower bound and less than the current node' value.
+For the the right child, the child should be greater than the current node but less than the upper bound.
+
+In both solutions, I purposely stop checking the right side if the left side has already been determined to be
+invalid. In this way, I avoid validating both sides. I simply need to know if any side is invalid. If I knew
+the left side was invalid, then there is no need to check the right side.
+
+"""
+
 
 def is_valid_bst(root):
-    # return recursive_solution(root, float("-inf"), float("inf"))
-    return iterative_solution(root)
+    return recursive_solution(root, float("-inf"), float("inf"))
+    # return iterative_top_down_solution(root)
 
 
-def recursive_solution(root, lower_bound, higher_bound):
-    if not root:
+def recursive_solution(node, lower_bound, higher_bound):
+    if not node:
         return True
-    if root.data <= lower_bound or root.data >= higher_bound:
+    if node.data <= lower_bound or node.data >= higher_bound:
         return False
-    return recursive_solution(root.left, lower_bound, root.data) and recursive_solution(
-        root.right, root.data, higher_bound
-    )
+    if not recursive_solution(node.left, lower_bound, node.data):
+        return False
+    if not recursive_solution(node.right, node.data, higher_bound):
+        return False
+    return True
 
 
-def iterative_solution(root):
+# this solution uses space of n
+#
+def iterative_top_down_solution(root):
     node_and_bounds_stack = [(root, -float("inf"), float("inf"))]
 
     while len(node_and_bounds_stack):
@@ -34,6 +53,7 @@ def iterative_solution(root):
         if parent_node.data <= lower_bound or parent_node.data >= upper_bound:
             return False
 
+        # since the node is currently balanced, we need to check the left and right children
         if parent_node.left:
             node_and_bounds_stack.append(
                 (parent_node.left, lower_bound, parent_node.data)
@@ -43,4 +63,5 @@ def iterative_solution(root):
                 (parent_node.right, parent_node.data, upper_bound)
             )
 
+    # we only end up here after checking every node to validate that it is a BST
     return True
