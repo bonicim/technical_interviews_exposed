fc29e49edc8721ada58408b7739b38ffce42a6ca
Mark Bonicillo <markabonicillo@gmail.com>
2023-10-09 13:17:13 -0700

    Update find_minimum_rotated_sorted_array.py

--- src/algorithms/blind_curated_75_leetcode_questions/find_minimum_rotated_sorted_array.py
+++ src/algorithms/blind_curated_75_leetcode_questions/find_minimum_rotated_sorted_array.py
@@ -9,11 +9,46 @@ def find_min(nums):
     You may assume no duplicate exists in the array.
     """
 
+    res = nums[0]
+    left = 0
+    right = len(nums) - 1
+    
+    while left <= right:
+        # when the slice of the array is sorted, check the current result against the new minimum
+        if nums[left] < nums[right]:
+            res = min(res, nums[left])
+            break
+            
+        pivot_index = (right + left) // 2
+        pivot = nums[pivot_index]
+        # the pivot is also our guess of the lowest number in the array; so check it against the current result 
+        res = min(res, pivot)
+
+        # the pivot is part of the sorted array on the right, so search left because the array on the left will have smaller values
+        if pivot >= nums[left]:
+            left = pivot_index + 1
+        else:
+        # the pivot is not part of the sorted array on the left, so search the right 
+            right = pivot_index - 1
+
+    return res
+
+
+def find_min_pivot_index(nums):
+    """
+    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+    (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+    Find the minimum element.
+
+    You may assume no duplicate exists in the array.
+    """
+
     # handles case of a sorted array that shifted by 0
     if nums[len(nums) - 1] > nums[0]:
         return nums[0]
 
-    first_num = nums[0]
     left = 0
     right = len(nums) - 1
     rotate_index = 0
@@ -22,8 +57,8 @@ def find_min(nums):
         pivot_index = (right + left) // 2
         pivot = nums[pivot_index]
 
-        if pivot >= first_num:
-            left = pivot_index
+        if pivot >= nums[left]:
+            left = pivot_index 
         else:
             right = pivot_index
 
