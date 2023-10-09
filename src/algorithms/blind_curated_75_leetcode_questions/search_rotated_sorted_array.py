3189ed6667d1cb6e767c9b2be330f406e00e727a
Mark Bonicillo <markabonicillo@gmail.com>
2023-10-09 14:39:01 -0700

    Update search_rotated_sorted_array.py

--- src/algorithms/blind_curated_75_leetcode_questions/search_rotated_sorted_array.py
+++ src/algorithms/blind_curated_75_leetcode_questions/search_rotated_sorted_array.py
@@ -1,4 +1,40 @@
-def search(nums, target):
+def search(self, nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) -1
+
+    while left <= right:
+        pivot_idx = (right + left) // 2
+        guess = nums[pivot_idx]
+
+        if guess == target:
+            return pivot_idx
+
+        # check if we are in left-sorted portion
+        if nums[left] <= guess:
+            
+
+            if target < nums[left]:
+                left = pivot_idx + 1
+            if target > guess or target < nums[left]:
+                # search right
+                left = pivot_idx + 1
+            else:
+                # target is in left-sorted portion of the array
+                # search right
+                right = pivot_idx - 1
+        else:
+            # right-sorted portion
+            if target > guess and target > nums[right]:
+                right = pivot_idx -1
+            elif target < guess or
+            else:
+                # search the right portion
+                left = pivot_idx + 1
+                
+    return -1 
+    
+        
+def search_old(nums, target):
     def find_rotation_point(nums):
         if nums[0] < nums[len(nums) - 1]:
             return 0
